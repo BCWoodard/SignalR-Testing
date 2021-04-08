99c994d8cbf5d17c62cc4aed111582816f525391
BCWoodard <bcwoodard@gmail.com>
2021-04-08 16:00:00 -0500

    Troubleshooting

--- SignalR-Testing/ViewController.swift
+++ SignalR-Testing/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var msgTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     
-    private let serverUrl = URL(string: "https://figo-api.ask.vet/FigoPetOwnerHub/")
+    private let serverUrl = URL(string: "https://figo-api.ask.vet/FigoPetOwnerHub")
     private var chatHubConnection: HubConnection?
     private var chatHubConnectionDelegate: HubConnectionDelegate?
     
@@ -35,19 +35,30 @@ class ViewController: UIViewController {
         self.chatHubConnectionDelegate = ChatHubConnectionDelegate(controller: self)
         self.chatHubConnection = HubConnectionBuilder(url: serverUrl!)
             .withLogging(minLogLevel: .debug)
-            .withAutoReconnect()
             .withHubConnectionDelegate(delegate: self.chatHubConnectionDelegate!)
             .withHttpConnectionOptions(configureHttpOptions: { (options) in
+//
+//          ### Partner Token
 //                options.accessTokenProvider = { "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJGSUdPIiwiZXhwIjoyNTM0MDIzMDA4MDAsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3QifQ.SysvET6OPaYF12rTYp252r4C0E7ZYcCSsJ51auLEL80" }
+//
+//          ### User token from getLiveVetConfiguration
+                
+//                options.headers = ["Authorization" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiSGFsZXkgSGFsYWsiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImQwMzIxYjYwLTgzMjItNGM0MC04NDNmLTYwMzI2NmQxMzU4YiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImhhbGV5LmhhbGFrQGdtYWlsLmNvbS54eCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJQZXRPd25lciIsIkZJR09fUEVUX09XTkVSIl0sImV4cCI6MTYxNzkxNTIyNCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3QiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdCJ9.GwiPj7fls_0rAUcEdNAvj0s7aOVJUax491y1jmS1-5s"]
                 options.accessTokenProvider = { "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiSGFsZXkgSGFsYWsiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImQwMzIxYjYwLTgzMjItNGM0MC04NDNmLTYwMzI2NmQxMzU4YiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImhhbGV5LmhhbGFrQGdtYWlsLmNvbS54eCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJQZXRPd25lciIsIkZJR09fUEVUX09XTkVSIl0sImV4cCI6MTYxNzkxNTIyNCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3QiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdCJ9.GwiPj7fls_0rAUcEdNAvj0s7aOVJUax491y1jmS1-5s" }
                 options.skipNegotiation = true
             })
             .build()
-        
-        self.chatHubConnection!.on(method: "JoinSession") { (responseObject) in
-            print("### JoinSession: \(responseObject)")
-        }
         self.chatHubConnection!.start()
+        
+        let intakeObject = AskVetIntakeObject(figoPetId: "19490370", question: "another string", neutered: false)
+        self.chatHubConnection!.on(method: "OnSessionChange") { (response) in
+            print("### JoinSession: \(response)")
+        }
+        self.chatHubConnection!.invoke(method: "JoinSession", intakeObject) { (err) in
+            print("### Err: \(err?.localizedDescription)")
+        }
+        
+
     }
 
 
@@ -77,7 +88,7 @@ class ViewController: UIViewController {
     @IBAction func btnSend(_ sender: UIButton) {
         let intakeObject = AskVetIntakeObject(figoPetId: "19490370", question: "another string", neutered: false)
         
-        chatHubConnection!.invoke(method: "JoinSession", intakeObject, invocationDidComplete: { (response) in
+        chatHubConnection!.invoke(method: "Post", intakeObject, invocationDidComplete: { (response) in
             print("\n### Invoke Session Call:\n \(String(describing: response))")
         })
     }
